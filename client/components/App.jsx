c41c3bb460692048394f9eba42da1a40f9a57753
SharleneRyan <sa.ryan@live.com>
2019-04-25 16:33:23 +1200

    adding component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,9 +4,22 @@ import ErrorMessage from './ErrorMessage'
 import LoadSubreddit from './LoadSubreddit'
 import SubredditList from './SubredditList'
 import WaitIndicator from './WaitIndicator'
+// import Header from './Header'
+import Button from './Button'
+// import Quote from './Quote'
+// import Sound from './Sound'
+// import Video from './video'
+
+
+
 
 const App = () => (
   <div className='app'>
+  {/* <Header/> */}
+  <Button/>
+  {/* <Quote/>
+  <Sound/>
+  <Video/> */}
     <ErrorMessage />
     <LoadSubreddit>
       <WaitIndicator />
---
+++ client/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+function Button  (){
+  return (
+    <div className="">
+    <button>
+      <a href="">Home</a>
+    </button>
+    </div>
+  )
+
+}
+
+
+
+export default Button
