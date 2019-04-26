7e6797fe4537d8d25aa5a36c678033e3c3ab17a7
SharleneRyan <sa.ryan@live.com>
2019-04-26 15:51:42 +1200

    adding video

--- client/components/video.jsx
+++ client/components/video.jsx
@@ -6,20 +6,20 @@ const Video = () => {
     <div >
     <Roll left>
     <div className="">
-    <iframe width="90%" height="600" src="https://www.youtube.com/embed/F3nZu5JrLK4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="100%" height="600" src="https://www.youtube.com/embed/F3nZu5JrLK4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
     </Roll>
     <div>
       <Roll right>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/jk-xUMvXvwk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="500" src="https://www.youtube.com/embed/jk-xUMvXvwk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Roll>
       <Roll left>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/R6FhEW7T7I0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="500" src="https://www.youtube.com/embed/R6FhEW7T7I0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Roll>
     </div>
     <div>
       <Roll right>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/R6FhEW7T7I0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="500" src="https://www.youtube.com/embed/R6FhEW7T7I0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Roll>
     </div>
 </div>
